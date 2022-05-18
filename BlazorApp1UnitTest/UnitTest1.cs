d9d85e20b93541644ebab87eae06edf84d44067f
Soumik Mukhopadhyay <soumik.mukhopadhyay@outlook.com>
2022-05-18 11:40:48 +0530

    added test case

--- BlazorApp1UnitTest/UnitTest1.cs
+++ BlazorApp1UnitTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Bunit;
+
 namespace BlazorApp1UnitTest
 {
     public class Tests
@@ -10,7 +12,15 @@ namespace BlazorApp1UnitTest
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            // Arrange
+            using var context = new Bunit.TestContext();
+
+            // Act
+            var renderComponent = context.RenderComponent<BlazorApp1.Pages.Index>();
+            renderComponent.Find("button").Click();
+
+            // Assert
+            renderComponent.Find("h5").MarkupMatches("<h5>Blazor component is clicked.</h5>");
         }
     }
 }
