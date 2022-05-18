34dba72ec684cdb03d31adc240fd3c7b2224c9ba
Soumik Mukhopadhyay <soumik.mukhopadhyay@outlook.com>
2022-05-18 12:41:57 +0530

    added CounterComponentTest

--- BlazorApp1UnitTest/UnitTest1.cs
+++ BlazorApp1UnitTest/UnitTest1.cs
@@ -10,17 +10,33 @@ namespace BlazorApp1UnitTest
         }
 
         [Test]
-        public void Test1()
+        public void SimpleTestComponent()
+        {
+            // Arrange : render the razor component
+            using var context = new Bunit.TestContext();
+            var renderComponent = context.RenderComponent<BlazorApp1.Pages.Index>();
+
+            // Act : find and click the <button> element to increment
+            renderComponent.Find("button").Click();
+
+            // Assert : first find the <h5> element, then verify its content
+            renderComponent.Find("h5").MarkupMatches("<h5>Blazor component is clicked.</h5>");
+        }
+
+        [Test]
+        public void CounterComponentTest()
         {
             // Arrange
             using var context = new Bunit.TestContext();
+            var component = context.RenderComponent<BlazorApp1.Pages.Counter>();
+            component.SetParametersAndRender(parameters => parameters.Add(p => p.Increment, 2));
 
             // Act
-            var renderComponent = context.RenderComponent<BlazorApp1.Pages.Index>();
-            renderComponent.Find("button").Click();
+            var button = component.Find("button");
+            button.Click();
 
-            // Assert
-            renderComponent.Find("h5").MarkupMatches("<h5>Blazor component is clicked.</h5>");
+            // Assert: Count should be 4
+            component.MarkupMatches(@"<p>Current count: 2</p><button>Click me</button>");
         }
     }
 }
